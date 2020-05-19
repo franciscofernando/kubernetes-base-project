aa2b000385c03302c50273c2fb867b23824d4726
Francisco Fernando <francisco1994@gmail.com>
2020-05-19 13:12:13 -0400

    add faileture control

--- simple-nodejs-http-server/index.js
+++ simple-nodejs-http-server/index.js
@@ -1,18 +1,34 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = Number(process.env.PORT);
 
+const envPath = '.env';
+const envvPath = '.envv';
+
 app.get('', (req, res) => {
-  res.send(process.env.SECRET);
+	res.send(process.env.SECRET);
 });
 
 app.get('/healthy', (req, res) => {
-  res.status(200).end();
+	res.status(fs.existsSync(envPath) ? 200 : 500).end();
+});
+
+app.get('/hide', (req, res) => {
+	fs.rename(envPath, envvPath);
+	console.log('.env hidden');
+	res.status(200).end();
+});
+
+app.get('/show', (req, res) => {
+	fs.rename(envvPath, envPath);
+	console.log('.env visible');
+	res.status(200).end();
 });
 
 app.listen(port, () => {
-  console.log('Logging in kubernetes pod, port: ' + port + '!');
+	console.log('Logging in kubernetes pod, port: ' + port + '!');
 });
